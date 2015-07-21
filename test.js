3b41ff9eca4150c06139cdf689cec12abb1786fa
thirdcoder <boris3rd@bk.ru>
2015-07-20 22:13:23 -0700

    Add more tests

--- test.js
+++ test.js
@@ -5,6 +5,18 @@ var nonary2n = require('./').nonary2n;
 var n2nonary = require('./').n2nonary;
 
 test('nonary2bts', function(t) {
+  t.equal(nonary2bts('0'), '00');
+  t.equal(nonary2bts('a'), '01');
+  t.equal(nonary2bts('b'), '1i');
+  t.equal(nonary2bts('c'), '10');
+  t.equal(nonary2bts('d'), '11');
+  t.equal(nonary2bts('w'), '0i');
+  t.equal(nonary2bts('x'), 'i1');
+  t.equal(nonary2bts('y'), 'i0');
+  t.equal(nonary2bts('z'), 'ii');
+
+  t.equal(nonary2bts('zzzzz'), 'iiiiiiiiii');
+  t.equal(nonary2bts('zzzdd'), 'iiiiii1111');
   t.equal(nonary2bts('zzz'), 'iiiiii');
   t.equal(nonary2bts('ddd'), '111111');
 
@@ -17,6 +29,8 @@ test('nonary2bts', function(t) {
 });
 
 test('bts2nonary', function(t) {
+  t.equal(bts2nonary('iiiiiiiiii'), 'zzzzz');
+  t.equal(bts2nonary('iiiiii1111'), 'zzzdd');
   t.equal(bts2nonary('0'), '0');
   t.equal(bts2nonary('i'), 'w');
   t.equal(bts2nonary('10'), 'c');
@@ -27,12 +41,17 @@ test('bts2nonary', function(t) {
 });
 
 test('nonary2n', function(t) {
+  t.equal(nonary2n('abcxyz'), 74165);
+  t.equal(nonary2n('zzz'), -364);
   t.equal(nonary2n('wdd'), 121);
   t.equal(nonary2n('zz'), -40);
+  t.equal(nonary2n('zzzzz'), -29524);
   t.end();
 });
 
 test('n2nonary', function(t) {
+  t.equal(n2nonary(74165), 'abcxyz');
+  t.equal(n2nonary(364), 'ddd');
   t.equal(n2nonary(-121), 'wzz');
   t.equal(n2nonary(-29524), 'zzzzz');
   t.equal(n2nonary(29524), 'ddddd');
